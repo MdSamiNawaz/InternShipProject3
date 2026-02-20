6f1fe635878dc98330cad31bcb4a97220bf39320
MdSamiNawaz <saminawaz4546@gmail.com>
2026-02-20 11:21:20 +0530

    sav

--- script.js
+++ script.js
@@ -32,7 +32,34 @@ headings.forEach((heading, index) => {
 });
 */
 
+ const words = [" Video Editing |", "Posters |", "Reels |", "Social Media |", "Social Media Management"];
+ const colors = ["#00ffee", "#d0ff00", "#ffd900", "#ff0059", "#00a6ff"];
+ const fonts = ["Arial", "Georgia", "Courier New", "Verdana", "Tahoma"];
  
+const styles = ["normal", "italic", "normal", "italic", "normal"];
+
+
+let index = 0;
+const text = document.getElementById("text");
+
+function changeWord() {
+    text.textContent = words[index];
+    text.style.color = colors[index];
+    text.style.fontFamily = fonts[index];
+    text.style.fontStyle = styles[index];
+     
+    index++;
+
+    if (index === words.length) {
+        index = 0;  
+    }
+}
+
+setInterval(changeWord,2000); 
+
+
+
+
 
 const Ny = document.querySelector(".year");
 
